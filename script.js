cfa239b7b6294f17a062a9562ad746f984157d3a
chi <chi@zhangs.me>
2021-08-24 11:41:53 +1200

    remove leaflet ajax, isntead load loi data once as json at start

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+const loiUrl = "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson";
 const countryCenter = [-41.6, 174.5];
 const countryZoom = 5;
 const localZoom = 14;
 const propsToExclude = ["id", "Added"];
+const loiIcon = L.Icon.extend({
+  options: {
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  },
+});
 
+const loadJson = async url => {
+  const response = await fetch(url);
+  if(!response.ok)
+    throw new Error(response.statusText);
+
+  const data = await response.json();
+  return data;
+}
 const Run = () => {
+  loadJson(loiUrl).then(data => {
+    renderMap(data);
+  }).catch(error => {
+    document.querySelector("#info").innerHTML = `
+      <p> 
+          Sorry, the data loading has failed. You can still check out the locations of interest information at the <a href='https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest'>Ministry of Health.</a>
+      </p> 
+    `
+  })
+}
+
+
+const renderMap = (data) => {
   //set the country at the map center with appropriate zoom level
   const map = L.map("map").setView(countryCenter, countryZoom);
 
@@ -20,8 +50,8 @@ const Run = () => {
   map.locate({ setView: true, maxZoom: localZoom });
 
   //geojson layer
-  const geojsonLayer = new L.GeoJSON.AJAX(
-    "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson",
+  const geojsonLayer = new L.geoJSON(
+    data,
     {
       pointToLayer: (feature, latlng) => {
         const props = feature.properties;
@@ -36,22 +66,18 @@ const Run = () => {
         }
         popupContent += "</table>";
 
-        const loiIcon = new L.Icon({
+        const defaultLoiIcon = new loiIcon({
           iconUrl:
             "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
           shadowUrl:
             "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
         });
 
         const popupOptions = {
           className: "loli-popup",
         };
 
-        return L.marker(latlng, { icon: loiIcon }).bindPopup(
+        return L.marker(latlng, { icon: defaultLoiIcon }).bindPopup(
           popupContent,
           popupOptions
         );
