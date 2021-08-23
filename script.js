475dd8518195004f9b9c3e6e955856c580ed6efc
chi <chi@zhangs.me>
2021-08-23 13:39:30 +1200

    change from openlayer to leaflet

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
 const Run = () => {
-  const getLocation = () => {   
-    console.log('getLocation was called') 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        hideLoadingDiv();
-        console.log('Geolocation is not supported by this device')
-    }
-}
+  const onLocationFound = (e) => {
+    L.marker(e.latlng).addTo(map);
+  };
+  var map = L.map("mapid").fitWorld();
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "something cool",
+  }).addTo(map);
 
-
-const showPosition = (p) => {
-    console.log('posiiton accepted');
-    console.log(p);
-    allowGeoRecall = false;
-}
-
-getLocation();
-
-
-  var vectorLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      projection: "EPSG:3857",
-      url: "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson",
-      format: new ol.format.GeoJSON(),
-    }),
-  });
-  var map = new ol.Map({
-    target: "map",
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-      vectorLayer,
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4,
-    }),
-  });
+  map.locate({ setView: true, maxZoom: 16 });
+  map.on("locationfound", onLocationFound);
+  const onEachFeature = (feature, layer) => {
+    console.log(e);
+  };
+  var geojsonLayer = new L.GeoJSON.AJAX(
+    "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson",
+    { onEachFeature: onEachFeature }
+  );
+  geojsonLayer.addTo(map);
 };
 
 window.onload = Run;
