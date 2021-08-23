672c4364b8b12e77cf6343f34affc4e68a2e5ac1
chi <chi@zhangs.me>
2021-08-23 18:12:28 +1200

    add properties popup

--- script.js
+++ script.js
@@ -2,19 +2,58 @@ const Run = () => {
   const onLocationFound = (e) => {
     L.marker(e.latlng).addTo(map);
   };
-  var map = L.map("mapid").fitWorld();
+  var map = L.map("map").fitWorld();
   L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "something cool",
   }).addTo(map);
 
   map.locate({ setView: true, maxZoom: 16 });
   map.on("locationfound", onLocationFound);
-  const onEachFeature = (feature, layer) => {
-    console.log(e);
-  };
   var geojsonLayer = new L.GeoJSON.AJAX(
     "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson",
-    { onEachFeature: onEachFeature }
+    {
+      pointToLayer: (feature, latlng) => {
+        const popupContent = `<table>
+                                <tr>
+                                  <td> Event: </td>
+                                  <td>${feature.properties.Event}</td>
+                                </tr>
+                                <tr>
+                                  <td> Location: </td>
+                                  <td>${feature.properties.Location}</td>
+                                </tr>
+                                <tr>
+                                  <td> Start: </td>
+                                  <td>${feature.properties.Start}</td>
+                                </tr>
+                                <tr>
+                                  <td> End: </td>
+                                  <td>${feature.properties.End}</td>
+                                </tr>
+                                <tr>
+                                  <td> Advice: </td>
+                                  <td>${feature.properties.Advice}</td>
+                                </tr>
+                           </table>`;
+        const popupOptions = {
+          className: "loli-popup",
+        };
+        var loiIcon = new L.Icon({
+          iconUrl:
+            "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
+          shadowUrl:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41],
+        });
+        return L.marker(latlng, { icon: loiIcon }).bindPopup(
+          popupContent,
+          popupOptions
+        );
+      },
+    }
   );
   geojsonLayer.addTo(map);
 };
